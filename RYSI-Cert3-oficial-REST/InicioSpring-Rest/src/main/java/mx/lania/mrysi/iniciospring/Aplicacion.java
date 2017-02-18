bcaf8068d59e8ce925e23b7b4f336cee4a9323cd
Julio Aguilar <julio.aguilar@lania.edu.mx>
2017-02-17 23:09:07 -0600

    Configuración básica de Sitemesh 3.

--- RYSI-Cert3-oficial-REST/InicioSpring-Rest/src/main/java/mx/lania/mrysi/iniciospring/Aplicacion.java
+++ RYSI-Cert3-oficial-REST/InicioSpring-Rest/src/main/java/mx/lania/mrysi/iniciospring/Aplicacion.java
@@ -5,14 +5,21 @@
  */
 package mx.lania.mrysi.iniciospring;
 
+import java.util.EnumSet;
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
+import org.sitemesh.builder.SiteMeshFilterBuilder;
+import org.sitemesh.config.ConfigurableSiteMeshFilter;
+import org.sitemesh.webapp.SiteMeshFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.DispatcherServlet;
 
 /**
@@ -24,12 +31,14 @@ public class Aplicacion implements WebApplicationInitializer {
     private final static Logger logger = LoggerFactory.getLogger(Aplicacion.class);
 
     @Override
-    public void onStartup(ServletContext contenedor) throws ServletException {
+    public void onStartup(ServletContext contextoServlets) throws ServletException {
         logger.debug("onStartup()");
         WebApplicationContext contextoSpring = getContext();
-        ServletRegistration.Dynamic servlet = contenedor.addServlet("despachador", new DispatcherServlet(contextoSpring));
+        ServletRegistration.Dynamic servlet = contextoServlets.addServlet("despachador", new DispatcherServlet(contextoSpring));
         servlet.setLoadOnStartup(2);
         servlet.addMapping("/");
+        
+        addFilters(contextoServlets);
     }
 
     private WebApplicationContext getContext() {
@@ -41,4 +50,23 @@ public class Aplicacion implements WebApplicationInitializer {
         contexto.setConfigLocation(paqueteClase + ".config");
         return contexto;
     }
+
+    private void addFilters(ServletContext contexto) {
+        // Filtro de encoding
+        EnumSet<DispatcherType> dispatcherTypes = EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD, DispatcherType.ERROR);
+
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+
+        FilterRegistration.Dynamic characterEncoding = contexto.addFilter("characterEncoding", characterEncodingFilter);
+        characterEncoding.addMappingForUrlPatterns(dispatcherTypes, true, "/*");
+        characterEncoding.setAsyncSupported(true);
+        
+        // Filtro de Sitemesh
+        ConfigurableSiteMeshFilter filtroSitemesh = new ConfigurableSiteMeshFilter();
+        FilterRegistration.Dynamic sitemesh = contexto.addFilter("sitemesh", filtroSitemesh);
+        sitemesh.addMappingForUrlPatterns(dispatcherTypes, true, "/*");
+    }
+
 }
---
+++ RYSI-Cert3-oficial-REST/InicioSpring-Rest/src/main/webapp/WEB-INF/decorador.jsp
@@ -0,0 +1,13 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Viajes / <sitemesh:write property='title'/></title>
+    <sitemesh:write property='head'/>
+</head>
+<body>
+    <div id='encabezado'>ENCABEZADO</div>
+    <div id='body'><sitemesh:write property='body'/></div>
+</body>
+</html>
